69042267e3dc19263d01a5fe74582fa2f226c23e
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-18 12:25:52 +0500

    Solve #task 4 Ternary conditions

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = 0;
+        if (first > second && first > third) {
+            result = first;
+        } else if (second > first && second > third) {
+            result = second;
+        } else {
+            result = third;
+        }
+        System.out.println(result);
     }
 }
